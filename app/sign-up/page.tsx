38d5b75ce58e8e3d16ab2924228d322f7bb0b31a
BadMood <andreiivanov.blag@gmail.com>
2026-07-07 13:20:43 +0300

    Added Libraries. Starting to create pages

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export default function SignUpPage() {
+    return <div className="flex min-h-screen flex-col bg-white">Sign UP Route</div>;
+}
